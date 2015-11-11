535d31184a61e3ae0aebcc1d8cf1838375aa7a5e
trikitrok <trikitrok@gmail.com>
2015-11-11 01:37:22 +0100

    Refactored 73 a bit

--- 73_4clojure.clj
+++ 73_4clojure.clj
@@ -1,4 +1,9 @@
 ;; 4clojure 73 Analyze a Tic-Tac-Toe Board
+
+[[:x :e :o]
+ [:x :e :e]
+ [:x :e :o]]
+
 ((partial apply (partial map (fn [a b c] [a b c])))
  [[:x :e :o]
   [:x :e :e]
@@ -11,6 +16,7 @@
 (columns [[:x :e :o]
           [:x :e :e]
           [:x :e :o]])
+
 
 (defn any-complete-column? [board mark]
   (some (fn [column]
@@ -35,31 +41,31 @@
   [:x :e :o]]
  :o)
 
-(defn any-complete? [coll mark]
+(defn any-complete? [mark coll]
   (some (fn [column]
           (= 3 (count (filter #(= mark %) column))))
         coll))
 
 (any-complete?
+ :x
  (columns
   [[:x :e :o]
    [:x :e :e]
-   [:x :e :o]])
- :x)
+   [:x :e :o]]))
 
 (any-complete?
+ :x
  (rows
   [[:x :e :o]
    [:x :e :e]
-   [:x :e :o]])
- :x)
+   [:x :e :o]]))
 
 (any-complete?
+ :x
  (rows
   [[:x :e :o]
    [:x :x :x]
-   [:x :e :o]])
- :x)
+   [:x :e :o]]))
 
 ; diag 1
 (get-in
@@ -93,10 +99,6 @@
   [:x :x :x]
   [3 :e :o]] [2 0])
 
-(def diag1 [[0 0] [1 1] [2 2]])
-(def diag2 [[0 2] [1 1] [2 0]])
-
-
 (defn diagonals [board]
   [(map #(get-in board %) [[0 0] [1 1] [2 2]])
    (map #(get-in board %) [[0 2] [1 1] [2 0]])])
@@ -106,18 +108,40 @@
   [3 4 5]
   [6 7 8]])
 
+((fn [board mark]
+  (some (partial any-complete? mark)
+        [(diagonals board) (columns board) (rows board)]))
+  [[:x :e :o]
+  [:x :o :x]
+  [:x :e 3]]
+ :x)
+
+((fn [board mark]
+  (some (partial any-complete? mark)
+        [(diagonals board) (columns board) (rows board)]))
+  [[:x :e :o]
+  [:o :o :x]
+  [:x :e 3]]
+ :x)
+
+((fn [board mark]
+  (some (partial any-complete? mark)
+        [(diagonals board) (columns board) (rows board)]))
+  [[:x :e :o]
+  [:o :o :x]
+  [:x :x :x]]
+ :x)
+
 (defn who-wins? [board]
-  (let [columns (partial apply (partial map (fn [a b c] [a b c])))
+  (let [diagonals-idxs [[[0 0] [1 1] [2 2]] [[0 2] [1 1] [2 0]]]
+        diagonal (partial map #(get-in board %))
+        columns (partial apply (partial map (fn [a b c] [a b c])))
         rows identity
-        diagonals (fn diagonals [board]
-                    [(map #(get-in board %) [[0 0] [1 1] [2 2]])
-                     (map #(get-in board %) [[0 2] [1 1] [2 0]])])
-        any-complete? (fn [coll mark]
+        any-complete? (fn [mark coll]
                         (some (fn [cell] (= 3 (count (filter #(= mark %) cell)))) coll))
         wins? (fn [board mark]
-                (or (any-complete? (diagonals board) mark)
-                    (any-complete? (columns board) mark)
-                    (any-complete? (rows board) mark)))]
+                (some (partial any-complete? mark)
+                      [(map diagonal diagonals-idxs) (columns board) (rows board)]))]
     (cond (wins? board :x) :x
           (wins? board :o) :o
           :else nil)))
@@ -127,9 +151,9 @@
             [:e :e :e]])
 
 (who-wins? [[:x :e :o]
-           [:x :e :e]
-           [:x :e :o]])
+            [:x :e :e]
+            [:x :e :o]])
 
 (who-wins? [[:e :x :e]
-           [:o :o :o]
-           [:x :e :x]])
+            [:o :o :o]
+            [:x :e :x]])
